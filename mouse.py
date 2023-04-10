cb3839272ba342287afb30eb0675b0b602b8f09e
Emil <emil88pl@gmail.com>
2023-04-10 10:34:43 +0100

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -5,6 +5,6 @@ import random
 while True:
     # move the mouse cursor slightly
     pyautogui.moveTo(random.randint(0,100), random.randint(0,720))
-    # wait for 2 minutes before moving again
+    # wait for X sec before moving again
     time.sleep(12)
 print('knock knock')
